da26da486eab561dabdd19c4f070a5b257ec94ff
MaximPixel <maximpixel@yandex.ru>
2020-02-26 17:42:23 +0200

    New version

--- launcher.lua
+++ launcher.lua
@@ -1,5 +1,6 @@
 local VERSION_DIR = "https://raw.githubusercontent.com/MaximPixel/CCLauncher/master/version.txt"
-local CURRENT_VERSION = 0
+local LAUNCHER_DIR = "https://raw.githubusercontent.com/MaximPixel/CCLauncher/master/launcher.lua"
+local CURRENT_VERSION = 1
 
 data = http.get(VERSION_DIR)
 
@@ -23,17 +24,25 @@ function getLatestVersion()
 	return -1
 end
 
-if data then
-	version = data.readAll()
-
-	if version then
-		print(version)
-		print(checkVersion(version, CURRENT_VERSION))
-	else
-		print("Failed to load")
+function downloadLatest()
+	data = http.get(LAUNCHER_DIR)
+	
+	if data then
+		text = data.readAll()
+		if text then
+			file = fs.open("launcher.lua", "w")
+			file.write(text)
+			file.close()
+			print("Updated!")
+		end
 	end
+	return false
+end
 
-	data.close()
-else
-	print("Failed to load")
+latestVersion = getLatestVersion()
+
+if latestVersion >= 0 then
+	if latestVersion > CURRENT_VERSION then
+		downloadLatest()
+	end
 end
