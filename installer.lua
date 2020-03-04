ecbce331ce7baf86336ec2d490b47f6539392163
MaximPixel <maximpixel@yandex.com>
2020-03-04 11:46:19 +0200

    Commit

--- installer.lua
+++ installer.lua
@@ -1,17 +1,27 @@
 local FILES_DIR = "https://raw.githubusercontent.com/MaximPixel/CCLauncher/master/files.xml"
 
-filesData = http.get(FILES_DIR)
-
-if filesData then
-	filesText = filesData.readAll()
-	if filesText then
-		filesList = textutils.unserialise(filesText)
-		if filesList then
-			print(filesList.version)
-			
-			for _, i in ipairs(filesList.files) do
-				print(i)
-			end
+function getFilesData()
+	filesData = http.get(FILES_DIR)
+	
+	if filesData then
+		filesText = filesData.readAll()
+		if filesText then
+				filesList = textutils.unserialise(filesText)
+				if filesList then
+					out = {}
+					print(filesList.version)
+					for _,i in ipairs(filesList.files) do
+						table.insert(out, i)
+					end;
+					return out
+				end
 		end
 	end
+	return nil
+end
+
+filesL = getFilesData()
+
+for _,i in ipairs(filesL) do
+	print(i)
 end
